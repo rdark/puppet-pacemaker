91f5166a4446b61d8517ce01137591af1e914661
Richard Dark <richard@fohnet.co.uk>
2011-01-04 16:33:06 +0000

    updated ha::ip so it shouuld actually work..

--- manifests/ip.pp
+++ manifests/ip.pp
@@ -1,17 +1,18 @@
-define ha::ip($address, $stickiness="undef", $ensure = present) {
-	ha::primitive { "ha-ip-${address}":
-		class_name       => "ocf:heartbeat:IPaddr2",
-		monitor_interval => "20",
+# Note that setting stickiness by default to 1000 will ensure that failback does NOT happen by default - set this to a lower value if this is the desired behaviour
+define ha::ip($address, $stickiness="1000", $ensure = present) {
+	ha::crm::primitive { "ha-ip-${address}":
+		resource_type    => "ocf:heartbeat:IPaddr2",
+		monitor_interval => "10s",
 		ensure           => $ensure,
-		stickiness       => $stickiness,
+		resource_stickiness => $stickiness,
 	}
 	
 	if $ensure != absent {
-		ha::parameter { "ha-ip-${address}-ip":
+		ha::crm::parameter { "ha-ip-${address}-ip":
 			resource  => "ha-ip-${address}",
 			parameter => "ip",
 			value     => $address,
-			require   => Ha::Primitive["ha-ip-${address}"],
+			require   => Ha::Crm::Primitive["ha-ip-${address}"],
 		}
 	}
 }
